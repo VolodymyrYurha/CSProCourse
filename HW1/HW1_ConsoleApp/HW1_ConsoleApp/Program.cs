86a973afbf24f28ed4bac27f597ca73ca8733be5
Volodymyr <volodymyr.yurg@gmail.com>
2023-02-21 13:11:32 +0200

    just test commit, to check branch

--- HW1/HW1_ConsoleApp/HW1_ConsoleApp/Program.cs
+++ HW1/HW1_ConsoleApp/HW1_ConsoleApp/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Here is additional line");
+Console.WriteLine("Here is additional second line");
