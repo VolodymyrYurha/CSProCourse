d841d01f36b1f2e435001d92dd2d6589101982cd
Volodymyr <volodymyr.yurg@gmail.com>
2023-03-19 21:03:59 +0200

    Repositories architecture

---
+++ HW1/HW1-2/Logistic/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logistic.ConsoleClient.Repositories
+{
+    internal interface IRepository<TEntity>
+        where TEntity : class
+    {
+        void Create(List<TEntity> entities);
+
+        List<TEntity> Read(string filename);
+    }
+}
