03927dbec28aed416fb9fe7fb7c843db4ba69487
Volodymyr <volodymyr.yurg@gmail.com>
2023-03-19 20:25:39 +0200

    added new models

---
+++ HW1/HW1-2/Logistic/Models/Warehouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logistic.ConsoleClient.Models
+{
+    internal class Warehouse
+    {
+        public int Id { get; set; }
+
+        public List<Cargo>? Stock { get; set; }
+    }
+}
