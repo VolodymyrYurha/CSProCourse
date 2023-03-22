3ec0e7682f0b2b2fe39b808ede951f7383295439
Volodymyr <volodymyr.yurg@gmail.com>
2023-03-22 15:12:43 +0200

    Removed Scenarios & original Vehicle methods

--- HW1/HW1-2/Logistic/Program.cs
+++ HW1/HW1-2/Logistic/Program.cs
@@ -14,8 +14,6 @@ void Main()
     // а іншу відловить обробник помилок та виведе відповідне повідомлення помилки
     try
     {
-        //SuccessScenario();
-        //ExceptionScenario();
         var repoXmlVehicle = new XmlRepository<Vehicle>();
         var vehiclesDeserialized = repoXmlVehicle.Read("Vehicle_2023-03-22_14-53-26.xml");
         var repoInMemoryVehicle = new InMemoryRepository<Vehicle>(vehiclesDeserialized);
@@ -40,71 +38,4 @@ void Main()
     {
         Console.WriteLine(e.Message);
     }
-}
-
-void SuccessScenario()
-{
-    Console.WriteLine("Success Scenario:");
-
-    // I    Створіть транспортний засіб Vehicle (дані на ваш розсуд)
-    var vehicle = new Vehicle(VehicleType.Car, 1000, 2999.99f, "BC 7777 BO");
-
-    // II   Виведіть інформацію GetInformation по транспортному засобу на консоль
-    Console.WriteLine("Vehicle before loading:");
-    Console.WriteLine(vehicle.GetInformation());
-
-    // III  Створіть масив cargos з декількома (3-5) new Cargo
-    var cargoes = new List<Cargo>
-    {
-        new Cargo(100,  99.9f, "#0001AA"),
-        new Cargo(150, 199.9f, "#0001AB"),
-        new Cargo(200, 299.9f, "#0002AA"),
-        new Cargo(250, 399.9f, "#0003AA"),
-        new Cargo(300, 499.9f, "#0001AC"),
-    };
-
-    // IV   У циклі додайте усі Cargo з массиву cargos до Vehicle за допомогою методу LoadCargo (Без переповнення)
-    foreach (var cargo in cargoes)
-    {
-        vehicle.LoadCargo(cargo);
-    }
-
-    // V    Виведіть інформацію GetInformation по Vehicle на консоль
-    Console.WriteLine("Vehicle after loading:");
-    Console.WriteLine(vehicle.GetInformation());
-}
-
-void ExceptionScenario()
-{
-    Console.WriteLine("Exception Scenario:");
-
-    // I    Створіть транспортний засіб Vehicle (дані на ваш розсуд)
-    var vehicle = new Vehicle(VehicleType.Train, 100_000, 500_000.00f, "BC 0666 BO");
-
-    // II   Виведіть інформацію GetInformation по транспортному засобу на консоль
-    Console.WriteLine("Vehicle before loading:");
-    Console.WriteLine(vehicle.GetInformation());
-
-    // III  Створіть масив cargos з декількома (3-5) new Cargo
-    var cargoes = new List<Cargo>
-    {
-        new Cargo(10000,  9900.9f, "#0001XA"),
-        new Cargo(15000, 19900.9f, "#0001XB"),
-        new Cargo(20000, 29900.9f, "#0002XA"),
-        new Cargo(25000, 39900.9f, "#0003XA"),
-        new Cargo(30000, 49900.9f, "#0001XC"),
-        new Cargo(1,         2.0f, "#0001XS"),
-    };
-
-    // IV   У циклі додайте усі Cargo з массиву cargos до Vehicle за допомогою методу LoadCargo (Без переповнення)
-    foreach (var cargo in cargoes)
-    {
-        vehicle.LoadCargo(cargo);
-    }
-
-    // V    Виведіть інформацію GetInformation по Vehicle на консоль
-    // після завантаження останнього грузу функція викине помилку, тому її виконання припинеться
-    // (тому інфо. про перенавантаження транспорту переніс в саме повідомлення про помилку)
-    Console.WriteLine("Vehicle after loading:");
-    Console.WriteLine(vehicle.GetInformation());
 }
